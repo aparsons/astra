0dae6b52c3046c34fe676ab9b40456774d3e8baa
Adam Parsons <aparsons@users.noreply.github.com>
2024-11-01 02:21:16 +0000

    Refactor GitHubWebhook admin configuration

--- webhooks/admin.py
+++ webhooks/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import GitHubWebhook
 
-admin.site.register(GitHubWebhook)
+
+class GitHubWebhookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ['id', 'public_id', 'enabled', 'created_at', 'updated_at']
+    list_display_links = ['id', 'public_id']
+    list_filter = ['enabled', 'created_at', 'updated_at']
+    readonly_fields = ("created_at", "updated_at")
+    search_fields = ["public_id"]
+
+admin.site.register(GitHubWebhook, GitHubWebhookAdmin)
