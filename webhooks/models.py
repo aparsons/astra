a741ba59a0494fe800e2a9a25031c541e1ff58ac
Adam Parsons <aparsons@users.noreply.github.com>
2024-11-01 02:21:31 +0000

    Refactor GitHubWebhook model to include additional fields and update field types

--- webhooks/models.py
+++ webhooks/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 
 class GitHubWebhook(models.Model):
-    public_id = models.CharField(max_length=255, unique=True, db_index=True)
-    enabled = models.BooleanField(default=True)
+    public_id = models.SlugField(unique=True, db_index=True)
+    enabled = models.BooleanField(default=True, db_index=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False, db_index=True)
 
     objects = models.Manager()
+
+    class Meta:
+        get_latest_by = "updated_at"
+        ordering = ["-updated_at"]
+        verbose_name = "GitHub Webhook"
+        verbose_name_plural = "GitHub Webhooks"
+
+    def __str__(self):
+        return self.public_id
