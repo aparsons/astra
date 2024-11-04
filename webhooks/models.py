67a12dae0b5eb040f41a844dbad58922ffb7b2dd
Adam Parsons <aparsons@users.noreply.github.com>
2024-11-04 02:32:06 +0000

    Refactor GitHubWebhook model to use EncryptedTextField for client_secret and add translation for verbose names

--- webhooks/models.py
+++ webhooks/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.forms import PasswordInput
+from django.utils.translation import gettext as _
 
-from encryption.fields import EncryptedCharField, EncryptedTextField
+from encryption.fields import EncryptedTextField
 
 class GitHubWebhook(models.Model):
     public_id = models.SlugField(unique=True, db_index=True)
     client_id = EncryptedTextField()
-    client_secret = EncryptedCharField(max_length=255)
     enabled = models.BooleanField(default=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, editable=False, db_index=True)
@@ -15,8 +16,8 @@ class GitHubWebhook(models.Model):
     class Meta:
         get_latest_by = "updated_at"
         ordering = ["-updated_at"]
-        verbose_name = "GitHub Webhook"
-        verbose_name_plural = "GitHub Webhooks"
+        verbose_name = _("GitHub Webhook")
+        verbose_name_plural = _("GitHub Webhooks")
 
     def __str__(self):
         return self.public_id
