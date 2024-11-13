d3970937f99fc51501b0cfeb9939eb301652770f
Adam Parsons <aparsons@users.noreply.github.com>
2024-11-13 01:52:03 +0000

    Add CSRF exemption to handle_github_webhook view for GitHub requests

--- webhooks/views.py
+++ webhooks/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import GitHubWebhook
 
@@ -17,7 +18,10 @@ def index(request: HttpRequest) -> HttpResponse:
     logger.debug("Index page accessed.")
     return HttpResponse("Hello, world. You're at the webhooks index.")
 
-
+# This is the view that will handle the GitHub webhook requests.
+# The @csrf_exempt decorator is used to disable CSRF protection for this view.
+# This is necessary because GitHub webhook requests do not include the CSRF token.
+@csrf_exempt
 def handle_github_webhook(request: HttpRequest, public_id: str) -> HttpResponse:
     if request.method == "POST":
         webhook = get_object_or_404(GitHubWebhook, public_id=public_id, enabled=True)
