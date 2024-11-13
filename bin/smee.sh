37b43751e29ffecec3d4a51bd210913a3ab71351
Adam Parsons <aparsons@users.noreply.github.com>
2024-11-13 01:51:34 +0000

    Update smee.sh to change webhook target URL for GitHub integration

--- bin/smee.sh
+++ bin/smee.sh
@@ -3,4 +3,4 @@
 docker run --rm --interactive --tty --name smee-client \
   deltaprojects/smee-client \
   --url https://smee.io/NP59jhHPw2Ujd2ML \
-  --target http://localhost:8000/webhooks/123/handle
+  --target http://localhost:8000/webhooks/github/123/handle
