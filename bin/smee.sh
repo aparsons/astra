a818994589d88ed42c6f1c81d46056edd86d311d
Adam Parsons <aparsons@users.noreply.github.com>
2024-11-21 01:43:40 +0000

    Check for smee-client installation and install if missing before starting

--- bin/smee.sh
+++ bin/smee.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker run --rm --interactive --tty --name smee-client \
-  deltaprojects/smee-client \
-  --url https://smee.io/NP59jhHPw2Ujd2ML \
-  --target http://localhost:8000/webhooks/github/123/handle
+# Check if smee is installed
+if ! command -v smee &> /dev/null
+then
+    echo "smee-client could not be found, installing..."
+    npm install --global smee-client
+fi
+
+# Start smee
+smee --url https://smee.io/NP59jhHPw2Ujd2ML --target http://localhost:8000/webhooks/github/123/handle
+
