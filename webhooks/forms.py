e0c7ef55f4c79aacd5e3c8c5d7a5f1870ec97cf5
Adam Parsons <aparsons@users.noreply.github.com>
2024-12-04 01:29:01 +0000

    Remove unused GitHubWebhookEventForm from webhooks/forms.py

--- webhooks/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class GitHubWebhookEventForm(forms.Form):
-    delivery_uuid = forms.UUIDField()
-    event = forms.CharField(max_length=255)
-    action = forms.CharField(max_length=255)
-    payload = forms.JSONField()
